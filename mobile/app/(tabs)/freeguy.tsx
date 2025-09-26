a8d9055eee70263b9bcbc5e6cdfb7a4f2544641a
santoswaitherzx <christopher.whaither.santos@escola.pr.gov.br>
2025-09-26 12:13:30 -0300

    adicionando o backgroud

---
+++ mobile/app/(tabs)/freeguy.tsx
@@ -0,0 +1,49 @@
+import { StyleSheet, View } from 'react-native';
+
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+
+export default function HomeScreen() {
+  return (
+        <View style={styles.container}>
+          <ThemedView style={styles.titleContainer}>
+            <ThemedText type="title"></ThemedText>
+          </ThemedView>
+    
+          <ThemedView style={styles.stepContainer}>
+            <ThemedText type="subtitle"></ThemedText>
+          </ThemedView>
+
+        
+      
+        </View>
+      );
+    }
+    
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: '#2D2828', 
+      },
+
+      titleContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 8,
+        backgroundColor: '#2D2828'
+      },
+
+      stepContainer: {
+        gap: 8,
+        marginBottom: 8,
+        backgroundColor: '#2D2828'
+      },
+
+      reactLogo: {
+        height: 280,
+        width: 550,
+        bottom: 0,
+        left: 0,
+        position: 'absolute',
+      },
+    });
