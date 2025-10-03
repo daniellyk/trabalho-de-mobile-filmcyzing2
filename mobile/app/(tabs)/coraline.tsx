4f9f092b1ea3e844dbcb0ad2778de02741b44acd
dudabastos1909 <rosa.maria30@escola.pr.gov.br>
2025-10-03 12:02:41 -0300

    foi alterada a fonte

--- mobile/app/(tabs)/coraline.tsx
+++ mobile/app/(tabs)/coraline.tsx
@@ -5,45 +5,56 @@ import { ThemedView } from '@/components/themed-view';
 
 export default function HomeScreen() {
   return (
-        <View style={styles.container}>
-          <ThemedView style={styles.titleContainer}>
-            <ThemedText type="title"></ThemedText>
-          </ThemedView>
+    <View style={styles.container}>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title"></ThemedText>
+      </ThemedView>
+  
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle"></ThemedText>
+      </ThemedView>
+  
+      <ThemedText style={styles.headerText}>
+        CORALINE
+      </ThemedText>
+      <ThemedText>Texto</ThemedText>
+    </View>
+  );
+}
+  
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2D2828', 
+  },
+
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+    backgroundColor: '#2D2828'
+  },
+
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+    backgroundColor: '#2D2828'
+  },
+  
+  headerText: {
+    fontSize: 32, 
+    fontWeight: 'bold',
+    color: '#7d0d7d', 
+    textAlign: 'center', 
+    marginTop: 20, 
+  },
+  
+  reactLogo: {
+    height: 280,
+    width: 550,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
     
-          <ThemedView style={styles.stepContainer}>
-            <ThemedText type="subtitle"></ThemedText>
-          </ThemedView>
-
-        
-      
-        </View>
-      );
-    }
-    
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: '#2D2828', 
-      },
-
-      titleContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 8,
-        backgroundColor: '#2D2828'
-      },
-
-      stepContainer: {
-        gap: 8,
-        marginBottom: 8,
-        backgroundColor: '#2D2828'
-      },
-
-      reactLogo: {
-        height: 280,
-        width: 550,
-        bottom: 0,
-        left: 0,
-        position: 'absolute',
-      },
-    });
