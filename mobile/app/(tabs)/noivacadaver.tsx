407382dcf11005b1a7b098ed1bd30e340f2744b3
Jorgk1ng7 <skyhunger875@gmail.com>
2025-10-03 12:04:50 -0300

    fonte foi alterada

--- mobile/app/(tabs)/noivacadaver.tsx
+++ mobile/app/(tabs)/noivacadaver.tsx
@@ -5,45 +5,55 @@ import { ThemedView } from '@/components/themed-view';
 
 export default function HomeScreen() {
   return (
-        <View style={styles.container}>
-          <ThemedView style={styles.titleContainer}>
-            <ThemedText type="title"></ThemedText>
-          </ThemedView>
+    <View style={styles.container}>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title"></ThemedText>
+      </ThemedView>
+  
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle"></ThemedText>
+      </ThemedView>
+  
+      <ThemedText style={styles.headerText}>
+      Noiva Cadarver
+      </ThemedText>
     
-          <ThemedView style={styles.stepContainer}>
-            <ThemedText type="subtitle"></ThemedText>
-          </ThemedView>
+    </View>
+  );
+}
+  
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2D2828', 
+  },
 
-        
-      
-        </View>
-      );
-    }
-    
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: '#2D2828', 
-      },
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+    backgroundColor: '#2D2828'
+  },
 
-      titleContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        gap: 8,
-        backgroundColor: '#2D2828'
-      },
-
-      stepContainer: {
-        gap: 8,
-        marginBottom: 8,
-        backgroundColor: '#2D2828'
-      },
-
-      reactLogo: {
-        height: 280,
-        width: 550,
-        bottom: 0,
-        left: 0,
-        position: 'absolute',
-      },
-    });
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+    backgroundColor: '#2D2828'
+  },
+  
+  headerText: {
+    fontSize: 32, 
+    fontWeight: 'bold',
+    color: '#772a53', 
+    textAlign: 'center', 
+    marginTop: 20, 
+  },
+  
+  reactLogo: {
+    height: 280,
+    width: 550,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
