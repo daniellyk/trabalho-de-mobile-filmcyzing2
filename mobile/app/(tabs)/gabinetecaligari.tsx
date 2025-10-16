6770bc7c7ac03e3a4c8fef9a98ed77b5d96c2581
MAYKOOOOOOO <maykon.salvador@escola.pr.gov.br>
2025-10-16 11:44:04 -0300

    ajustando o texto e a imagem

--- mobile/app/(tabs)/gabinetecaligari.tsx
+++ mobile/app/(tabs)/gabinetecaligari.tsx
@@ -1,16 +1,11 @@
-import { Image, StyleSheet, View } from 'react-native'; // Importando Image
+import { Image, ScrollView, StyleSheet, View } from 'react-native';
 
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-
-      <Image
-        style={styles.tinyLogo}
-        source={require('@/assets/images/hq720.jpg')}
-      />
+    <ScrollView style={styles.container}>
     
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title"></ThemedText>
@@ -23,22 +18,34 @@ export default function HomeScreen() {
       <ThemedText type='title' style={styles.headerText}>
         O Gabinete do Dr. Caligari
       </ThemedText>
+      <View style={styles.contentRow}>
 
-      <center> <ThemedText type="subtitle">Informações Gerais</ThemedText></center>
-      <ThemedText> Lançamento: 26 de fevereiro de 1920 </ThemedText>
-      <ThemedText> Gênero: Horror, Mudo, Terror (Expressionismo Alemão)</ThemedText>
-      <ThemedText> Diretor: Robert Wiene </ThemedText>
-      <ThemedText> Roteiro: Hans Janowitz e Carl Mayer </ThemedText>
-      <ThemedText> Duração: Aproximadamente 77 minutos </ThemedText>
-      <ThemedText> Faturamento: 18.000 USD </ThemedText>
-      <ThemedText> Onde Assistir: Telecine, Amazon Prime </ThemedText>
-      <ThemedText />
+
+        <View style={styles.posterColumn}>
+          <Image
+            style={styles.tinyLogo}
+            source={require('@/assets/images/capaexpresscabinetcaligari.jpg')}
+          />
+        </View>
+        <View style={styles.detailsColumn}>
+          <center> <ThemedText type="subtitle">Informações Gerais</ThemedText></center>
+          <ThemedText> Lançamento: 26 de fevereiro de 1920 </ThemedText>
+          <ThemedText> Gênero: Horror, Mudo, Terror (Expressionismo Alemão)</ThemedText>
+          <ThemedText> Diretor: Robert Wiene </ThemedText>
+          <ThemedText> Roteiro: Hans Janowitz e Carl Mayer </ThemedText>
+          <ThemedText> Duração: Aproximadamente 77 minutos </ThemedText>
+          <ThemedText> Faturamento: 18.000 USD </ThemedText>
+          <ThemedText> Onde Assistir: Telecine, Amazon Prime </ThemedText>
+          <ThemedText />
+        </View>
+      </View>
+
       <center> <ThemedText type="subtitle">Sinopse</ThemedText></center>
       <ThemedText> A história é contada do ponto de vista de Francis, um jovem que perdeu seu melhor amigo e suspeita que o Dr. Caligari e Cesare estão por trás dos crimes. À medida que Francis investiga, a linha entre a realidade e o delírio começa a se desvanecer, levando o espectador a questionar tudo o que vê na tela.
 
         O que torna "O Gabinete do Dr. Caligari" único não é apenas o enredo, mas a forma como ele é apresentado. O filme é uma obra-prima do expressionismo alemão, usando cenários distorcidos, ângulos de câmera incomuns e uma estética visual que reflete o estado mental perturbado dos personagens e o terror da história. </ThemedText>
-      
-    </View>
+
+    </ScrollView>
 
   );
 }
@@ -47,6 +54,24 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000000',
+    padding: 10,
+  },
+
+  contentRow: {
+    flexDirection: 'row',
+    marginBottom: 20,
+  },
+
+  posterColumn: {
+    flex: 1,
+    alignItems: 'center',
+    paddingRight: 10,
+  },
+
+  detailsColumn: {
+    flex: 2,
+    paddingLeft: 10,
+    justifyContent: 'flex-start',
   },
 
   titleContainer: {
@@ -63,6 +88,7 @@ const styles = StyleSheet.create({
   },
 
   headerText: {
+    padding: 50,
     fontSize: 32,
     fontWeight: 'bold',
     color: '#800000',
@@ -79,7 +105,8 @@ const styles = StyleSheet.create({
 
   },
   tinyLogo: {
-    width: 50,
-    height: 50,
+    width: '150%',
+    height: 220,
+    resizeMode: 'contain',
   },
 });
