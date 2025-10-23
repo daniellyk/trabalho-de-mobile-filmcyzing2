69d605c6152a3da53a5edbbfb7f0d039e5d78087
santoswaitherzx <christopher.whaither.santos@escola.pr.gov.br>
2025-10-23 12:10:09 -0300

    up

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,24 +1,34 @@
-import { Image, ScrollView, StyleSheet, View } from 'react-native';
-
+import { Image, StyleSheet, View } from 'react-native';
 
 import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
 
 export default function HomeScreen() {
   return (
+    <View style={styles.container}>
 
-    <ScrollView style={styles.container}>
+    
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title"></ThemedText>
+      </ThemedView>
+
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle"></ThemedText>
+      </ThemedView>
+
 
       <ThemedText style={styles.headerText}>
-        Filmcyzing
+      Filmcyzing
+
       </ThemedText>
 
       <View style={styles.gridContainer}>
-        
+
 
         <View style={styles.imagePair}>
           <Image
             style={styles.gridImage}
-            source={require('@/assets/images/miguel.jpg')}
+            source={require('@/assets/images/Miguel.webp')}
           />
           <Image
             style={styles.gridImage}
@@ -27,7 +37,7 @@ export default function HomeScreen() {
         </View>
 
 
-                <View style={styles.imagePair}>
+        <View style={styles.imagePair}>
           <Image
             style={styles.gridImage}
             source={require('@/assets/images/ouijaorigem.jpeg')}
@@ -44,27 +54,41 @@ export default function HomeScreen() {
             style={styles.gridImage}
             source={require('@/assets/images/Noivacadaver2.jpg')}
           />
+          <Image
+            style={styles.imagePair}
+            source={require('@/assets/images/coraline.jpg')}
+          />
 
-          <View style={styles.gridImage} /> 
+          <View style={styles.gridImage} />
         </View>
 
       </View>
 
       <View style={{ height: 50 }} />
-      
-    </ScrollView>
+
+    </View>
   );
 }
-
-
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
+    backgroundColor: '000000',
   },
 
-  
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+    backgroundColor: '000000'
+  },
+
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+    backgroundColor: '000000'
+  },
+
   headerText: {
     fontSize: 32,
     fontWeight: 'bold',
@@ -73,7 +97,7 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginBottom: 20,
   },
-  
+
 
   gridContainer: {
     paddingHorizontal: 10,
@@ -82,16 +106,17 @@ const styles = StyleSheet.create({
 
 
   imagePair: {
-    flexDirection: 'row', 
+    flexDirection: 'row',
     justifyContent: 'space-between',
-    marginBottom: 10, 
+    marginBottom: 15,
   },
 
   gridImage: {
 
-    width: '48%', 
-    height: 150, 
+    width: '50%',
+    height: 150,
     borderRadius: 8,
 
   },
-});
+  },
+);
