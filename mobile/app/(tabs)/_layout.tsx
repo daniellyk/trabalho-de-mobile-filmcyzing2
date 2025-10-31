8be1048f9266df0edc75ac04bbba3dcc51a45e1f
santoswaitherzx <christopher.whaither.santos@escola.pr.gov.br>
2025-10-31 11:11:27 -0300

    botões mudados.

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,10 +1,12 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
 import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import { Tabs } from 'expo-router';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -28,6 +30,48 @@ export default function TabLayout() {
         options={{
           title: 'Explore',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+       <Tabs.Screen
+        name="pixels"
+        options={{
+          title: 'Pixels',
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="pac-man" size={24} color="yellow" />
+        }}
+      />
+      <Tabs.Screen
+        name="ouija"
+        options={{
+          title: 'Ouija',
+          tabBarIcon: ({ color }) => <FontAwesome6 name="chess-board" size={24} color="red" />
+        }}
+      />
+      <Tabs.Screen
+        name="freeguy"
+        options={{
+          title: 'freeguy',
+          tabBarIcon: ({ color }) => <AntDesign name="aim" size={24} color="blue" />
+        }}
+      />
+      <Tabs.Screen
+        name="coraline"
+        options={{
+          title: 'coraline',
+          tabBarIcon: ({ color }) => <FontAwesome5 name="house-user" size={24} color="purple" />
+        }}
+      />
+       <Tabs.Screen
+        name="noivacadaver"
+        options={{
+          title: 'noivacadaver',
+          tabBarIcon: ({ color }) => <MaterialCommunityIcons name="bottle-tonic-skull-outline" size={24} color="gray" />
+        }}
+      />
+        <Tabs.Screen
+        name="gabinetecaligari"
+        options={{
+          title: 'gabinetecaligari',
+          tabBarIcon: ({ color }) => <FontAwesome5 name="cross" size={24} color="brown" />
         }}
       />
     </Tabs>
